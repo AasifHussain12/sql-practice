e841387c6cabec3f3bce188aa638358512dba19c
Aasif Hussain <asiff4268@gmail.com>
2026-09-19 15:23:03 +0530

    Add 02_create_table.sql

---
+++ 02_create_table.sql
@@ -0,0 +1,12 @@
+-- Create Table
+
+USE StudentDB;
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Age INT,
+    Gender VARCHAR(10),
+    Course VARCHAR(50),
+    City VARCHAR(50)
+);
