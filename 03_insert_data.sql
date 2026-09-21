92b1d7080dfeeee93794406ec44546570ceb21ee
Aasif Hussain <asiff4268@gmail.com>
2026-09-21 16:32:46 +0530

    Add 03_insert_data.sql

---
+++ 03_insert_data.sql
@@ -0,0 +1,9 @@
+-- Insert Data into Students Table
+
+INSERT INTO Students (Name, Age, Gender, Course, City)
+VALUES
+('Aasif Hussain', 24, 'Male', 'MSc IT', 'Pulwama'),
+('Ali', 22, 'Male', 'BSc IT', 'Srinagar'),
+('Sara', 23, 'Female', 'MCA', 'Anantnag'),
+('Amina', 21, 'Female', 'BCA', 'Baramulla')
+('Bilal', 25, 'Male', 'MSc CS', 'Kupwara');
