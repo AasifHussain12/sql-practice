6194565a633108e101de835bb491558bc81211cf
Aasif Hussain <asiff4268@gmail.com>
2026-09-24 11:35:43 +0530

    Update 03_insert_data.sql

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -5,5 +5,5 @@ VALUES
 ('Aasif Hussain', 24, 'Male', 'MSc IT', 'Pulwama'),
 ('Ali', 22, 'Male', 'BSc IT', 'Srinagar'),
 ('Sara', 23, 'Female', 'MCA', 'Anantnag'),
-('Amina', 21, 'Female', 'BCA', 'Baramulla')
+('Amina', 21, 'Female', 'BCA', 'Baramulla'),
 ('Bilal', 25, 'Male', 'MSc CS', 'Kupwara');
